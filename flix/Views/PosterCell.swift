881de4cbb16c1dfaf9488410ca5cc767ebe5443f
Allen Lozano <allenlozano@Allens-MacBook-Pro.local>
2018-09-27 19:13:16 -0700

    Added collection view of movies

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Allen Lozano on 9/27/18.
+//  Copyright © 2018 Allen Lozano. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
